8bb344407dcdfac466d44fc89844871ea0d00bf0
Lemuel Ayesu <lemuelayesu@gmail.com>
2025-02-24 16:09:23 +0000

    step by step

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,35 @@
-console.log(`Hello, World!`);
+let humanScore = 0;
+let computerScore = 0;
+
+//computer choice
+function getComputerChoice() {
+    let randomNUmber = Math.floor(Math.random() * 3);
+    if (randomNUmber === 0) {
+        return 'rock';
+
+    } else if (randomNUmber === 1) {
+        return 'paper';
+
+    } else {
+        return 'scissors';
+    }
+}
+
+//human choice
+function getHumanChoice() {
+    let humanChoice = prompt("Please type rock paper or scissors");
+    return humanChoice;
+
+}
+
+function playround(humanChoice, computerChoice) {
+    let humanChoice = getHumanChoice().toLowerCase;
+    let computerChoice = getComputerChoice().toLowerCase;
+
+    if (humanChoice === computerChoice) {
+        return 'It is a tie';
+
+
+    }
+
+}
